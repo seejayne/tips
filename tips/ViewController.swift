5a3357e6c2a389390a25def02d1ad4e57e6417c0
seejayne <jaynev@gmail.com>
2014-08-17 08:49:39 -0700

    Initial Upload to GitHub
    The initial upload

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
                             
+    @IBOutlet var tipLabel : UILabel
+    @IBOutlet var billField : UITextField
+    @IBOutlet var totalLabel : UILabel
+    @IBOutlet var tipControl : UISegmentedControl
+    @IBOutlet var twoLabel : UILabel
+    @IBOutlet var threeLabel : UILabel
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = " "
+        totalLabel.text = " "
+        self.tipLabel.alpha = 0
+        self.totalLabel.alpha = 0
+        self.twoLabel.alpha = 0
+        self.threeLabel.alpha = 0
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +33,39 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onEditingChanged(sender : AnyObject) {
+        var tipPercentages = [0.18,0.2,0.22]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        var billAmount = NSString(string: billField.text).doubleValue
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+        var divtwo = total / 2
+        var divthree = total / 3
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        twoLabel.text = "$\(divtwo)"
+        threeLabel.text = "$\(divthree)"
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        twoLabel.text = String(format: "$%.2f", divtwo)
+        threeLabel.text = String(format: "$%.2f", divthree)
+        
+        // Optionally initialize the property to a desired starting value
+        UIView.animateWithDuration(0.9, animations: {
+            // This causes first view to fade in and second view to fade out
+            self.tipLabel.alpha = 1
+            self.totalLabel.alpha = 1
+            self.twoLabel.alpha = 1
+            self.threeLabel.alpha = 1
+        
+            })
+    
+    }
+    @IBAction func onTap(sender : AnyObject) {
+    view.endEditing(true)
+    }
 }
 
